ef267799cfb395a00f1fd683e9d80ffc40409b37
gusttavo212 <gusttavo212@gmail.com>
2019-01-24 23:10:12 -0200

    connection implemented but not TEST TDD erro

---
+++ src/db/mongodb/mongodb.js
@@ -0,0 +1,43 @@
+const ICrud = require('./../interfaces/interfaceCrud');
+const mongoose = require('mongoose');
+const conectionSTATUS = {
+    0: "Disconectado",
+    1: "Conectado",
+    2: "Conectando",
+    3: "Disconectado"
+};
+
+class MongoDB extends ICrud {
+    constructor(connection, schema) {
+        super();
+        this._schema = schema;
+        this._connection = connection;
+    };
+
+    async isConnected() {
+        const state = conectionSTATUS[this._connection.readyState];
+        if (state === "Conectado") return state;
+
+        if (state !== "Conectando") return state;
+
+        await new Promise(resolve => setTimeout(resolve, 1000)); //Promise de conexão
+
+        return conectionSTATUS[this._connection.readyState];
+    };
+
+    static connect() {
+        mongoose.connect(
+            "mongodb://gadsden:250433@localhost/herois?authSource=admin&w=1",
+            { useNewUrlParser: true },
+            function(error) {
+              if (!error) return;
+              console.log("Falha na conexão", error);
+            }
+          );
+
+          const connection = mongoose.connection;
+          connection.once("open", () => console.log("Database MONGODB CONECTADO"));
+
+          return connection; //Manda para ICrud class gera o connection e o _connection recebe
+    }
+};
