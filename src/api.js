a3ab1b60e9dbbc5e02822532c7feb9a56e9b3847
gusttavo212 <gusttavo212@gmail.com>
2019-02-02 13:20:38 -0200

    API documentations config in API

--- src/api.js
+++ src/api.js
@@ -3,6 +3,10 @@ const Context = require('./../src/db/base/contextStrategy');
 const MongoDb = require('./../src/db/mongodb/mongodb');
 const ProductSchema = require('./../src/db/mongodb/schemas/productSchema');
 const ProductRoute = require('./../src/routes/productRoutes');
+//DOCUMENTATION
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
 
 const app = new hapi.Server({
     port: 5000
@@ -18,6 +22,27 @@ async function main() {
     app.route([
         ...mapRoutes(new ProductRoute(context), ProductRoute.methods())
     ]);
+
+    //DOCUMENTATION
+    const swaggerOptions = {
+        info: {
+            title: 'API Products  -#gusttavo212',
+            version: 'v1.0'
+        },
+        lang: 'pt'
+    }
+     //DOCUMENTATION
+     //Plugins HAPI
+    await app.register([        
+        Vision,
+        Inert,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ])
+    //Plugins HAPI
+   
 
     await app.start();
     console.log('Servidor rodando da porta', app.info.port);
